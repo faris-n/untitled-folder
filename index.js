468d875515170b09e32d24b89235047e59c6cd10
Faris Jaber <faris_jaber@Fariss-MacBook-Pro.local>
2023-02-13 16:49:28 +0200

    initial test

---
+++ index.js
@@ -0,0 +1,24 @@
+var express = require("express");
+var app = express();
+var PORT = 3000;
+
+// Without middleware
+app.get("/", function (req, res) {
+  // Setting the response
+  res.set("Content-Type", "application/javascript");
+
+  // "application/json"
+  console.log(res.get("Content-Type"));
+  res.status(200).send({
+    status: "success",
+    data: {
+      message: "API working fine",
+    },
+  });
+  res.end();
+});
+
+app.listen(3200, function (err) {
+  if (err) console.log(err);
+  console.log("Server listening on PORT", PORT);
+});
